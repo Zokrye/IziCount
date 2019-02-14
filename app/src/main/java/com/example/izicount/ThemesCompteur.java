88a32d2ae33e5bcb77a06f649e766a57a60f8386
Unknown <ALEXANDRE.CANTON@NEUF.FR>
2019-02-14 11:44:08 +0100

    Update

---
+++ app/src/main/java/com/example/izicount/ThemesCompteur.java
@@ -0,0 +1,14 @@
+package com.example.izicount;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ThemesCompteur extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_themes_compteurs);
+    }
+}
